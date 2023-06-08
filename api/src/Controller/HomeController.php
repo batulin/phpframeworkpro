aedba3984a850930dda5e3ba99f909c45e0dacce
batulin <batulin-1983@mail.ru>
2023-06-08 21:16:15 +0600

    Controller Autowiring

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Widget;
 use Framework\Http\Response;
 
 class HomeController
 {
+    public function __construct(private Widget $widget)
+    {
+    }
+
     public function index(): Response
     {
-        $content = '<h1>controller</h1>';
+        $content = "<h1>controller{$this->widget->name}</h1>";
         return new Response($content);
     }
 
